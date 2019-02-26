c5d971b0a0994681cf95d438980edb8c9574b3ed
Laboratória <ma-ah02@live.com>
2019-02-26 16:41:00 -0300

    Fazendo a lista de personagens

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-function getProducts(){
-    return list["data"];
+window.onload = function (){
+  showChampions();
+}
+
+function getChampion(){
+    return Object.values(list["data"]);
   }
-  console.log(getProducts());
+  console.log(getChampion());
+
+function showChampions(){
+  let championSection = document.getElementById("champion-list");
+  championSection.innerHTML = `
+  ${getChampion().map((champ) =>`
+      <section class="champion">
+        <img src ="${champ["img"]}" class="champ-img" />
+        <section>
+        <p class="champ-name"> ${champ["name"]} </p>
+        </section>
+      </section>
+    `).join("")}
+  `
+}
