75fa2821ee6656292316663f9bc9e5210dcc54e9
marisa <ma-ah02@live.com>
2019-03-04 13:47:02 -0300

    filtro funcionando

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 window.onload = function (){
-  showChampions();
+  getChampion();
+  showChampions(dataList);
 }
-//let arrayChamp = Object.values(LOL);
 
- function getChampion(){
+let dataList = Object.values(LOL["data"]);
+
+function getChampion(){
   LOL["data"]["Evelynn"]["img"] = "https://www.masterypoints.com/assets/img/lol/champion_icons/Evelynn.png";
   LOL["data"]["Leblanc"]["img"] = "https://www.masterypoints.com/assets/img/lol/champion_icons/Leblanc.png";
   LOL["data"]["TwistedFate"]["img"] = "https://www.masterypoints.com/assets/img/lol/champion_icons/TwistedFate.png";
-  return Object.values(LOL["data"]);
 }
 
-function showChampions(){
+function showChampions(currentArray){
   let championSection = document.getElementById("champion-list");
   championSection.innerHTML = `
-  ${getChampion().map((champ) =>`
+  ${currentArray.map((champ) =>`
       <section class="champion">
         <img src ="${champ["img"]}" id=${champ["id"]} class="champ-img" />
         <section>
@@ -36,7 +37,6 @@ function showProfile(event){
   let data = Object.values(LOL);
   let selectChamp = event.target.id;
   document.getElementById("profile").style.visibility = "visible";
-  console.log(data)
   document.getElementById("img-profile").src = data[3][selectChamp]["splash"];
   document.getElementById("profile-desc").innerHTML = data[3][selectChamp]["blurb"];
   document.getElementById("attack").innerHTML = "Attack: " + data[3][selectChamp]["info"]["attack"]
@@ -45,21 +45,33 @@ function showProfile(event){
   document.getElementById("difficulty").innerHTML = "Difficulty: " + data[3][selectChamp]["info"]["difficulty"]
   console.log(selectChamp);
 }
-//document.getElementById("btnCompare").addEventListener("click", compare);
+/*document.getElementById("btnCompare").addEventListener("click", compare);
 
-/*function compare(){
+function compare(){
+  if(document.getElementById("btnCompare").style.backgroundColor === "yellow"){
+    document.getElementById("btnCompare").style.backgroundColor = "grey";
+    document.getElementById("teste").innerHTML = "";
+    let button = document.getElementsByClassName("champ-img");
+    for (let i of button){
+      i.style.border = "2px solid grey"; 
+      i.addEventListener("click", showProfile)
+      }
+  }else{
+  document.getElementById("btnCompare").style.backgroundColor = "yellow";
   let button = document.getElementsByClassName("champ-img");
   for (let i of button){
     i.style.border = "2px solid grey"; 
     i.addEventListener("click", selectionChampion)
     }
+  }
 }
 function selectionChampion(event){
   let selected = event.target.id;
   let data = Object.values(LOL);
   let imagem = document.createElement("img");
   console.log(selected)
-  if(document.getElementById(selected).style.border == "2px solid blue"){
+  if(document.getElementById("btnCompare").style.backgroundColor === "yellow"){
+  if(document.getElementById(selected).style.border == "2px solid blue" ){
     document.getElementById("teste").removeChild(document.getElementById("teste").childNodes[0]);
     document.getElementById(selected).style.border = "2px solid grey"
   }else{
@@ -67,22 +79,19 @@ function selectionChampion(event){
     document.getElementById("teste").appendChild(imagem);
    document.getElementById(selected).style.border = "2px solid blue";
 }
+}
 }*/
 
 document.getElementById("filter2").addEventListener("change", filterSelection);
 
-
 function filterSelection(event) {
-  let idTag = filter2.value;
-  let data = getChampion();
-  let filtrado = data.filter((personagem) => {
+  let idTag = event.target.id;
+  if(idTag === "all"){
+    showChampions(dataList);
+  }else{
+  let filtrado = dataList.filter((personagem) => {
     return personagem.tags.includes(idTag);
   })
-  console.log(filtrado);
- 
+  showChampions(filtrado);
   }
-
-  /*function typeFilter(){
-    let filteredPokemon = POKEMON.pokemon.filter(pokemon => {
-    return (pokemon.type.includes(selectTypeElement.value));
-});*/
+  }
