a344e50ad5e47701c966141a0b5f6c4fd9289f5c
Laboratória <ma-ah02@live.com>
2019-07-17 17:52:33 -0300

    inicio das config do Chart.js

---
+++ src/components/Chart.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+import {Bar} from 'react-chartjs-2';
+
+export default function (props) {
+
+    const [arrayChamp, setArrayChamp] = useState({});
+    const [values, setValues] = useState([]);
+    const [allValuesChart, setAllValuesChart] = useState([]);
+
+    useEffect(() => {
+        setArrayChamp(props.chartValues);
+    }, [props.chartValues]);
+
+    useEffect(() => {
+        chartInit(arrayChamp);
+    }, [arrayChamp])
+
+
+    useEffect(() => {
+        console.log(values);
+    }, [values])
+
+
+    useEffect(() => {
+        arrayPerLevel();
+    }, [values])
+
+    function chartData () {
+        return {
+            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"],
+            datasets:[{
+              label: "",
+              data: [1,2],
+              borderWidth: 4,
+              borderColor: 'red',
+              backgroundColor: 'red'
+              }, 
+              {
+              label: "",
+              data: [1,2],
+              borderWidth: 4,
+              borderColor: 'green',
+              backgroundColor: 'green'
+              }, 
+              {
+              label: "",
+              data: [1,2],
+              borderWidth: 4,
+              borderColor: 'yellow',
+              backgroundColor: 'yellow'
+              }, 
+              {
+              label: "",
+              data: [1,2],
+              borderWidth: 4,
+              borderColor: 'blue',
+              backgroundColor: 'blue'
+              }, 
+              {
+              label: "",
+              data: [1,2],
+              borderWidth: 4,
+              borderColor: 'orange',
+              backgroundColor: 'orange'
+            }]
+        }
+    }
+
+    function chartInit(values){
+        let statInitial = Object.values(values)
+        .map((value) => value["stats"]["armor"]);
+        let valuesPerLvl = Object.values(values)
+        .map((value) => value["stats"]["armorperlevel"]);
+        setValues([statInitial, valuesPerLvl])
+     }
+
+    function arrayPerLevel() {
+        values
+        .map((teste) => teste.map((oi, index) => console.log(oi, index)))
+    }
+
+    return(
+        <div>
+            <Bar
+            data={chartData()}
+            options={{
+                maintainAspectRadio: false
+            }}
+            />
+        </div>
+    )
+
+}
--- src/pages/Champions.js
+++ src/pages/Champions.js
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react';
 import Modal from 'react-modal';
 import ChampModal from '../components/ChampModal'
 import ChampIcon from '../components/ChampIcon';
+import Chart from '../components/Chart';
 import './Champions.css'
 
 export default function (props) {
   const [data, setData] = useState([]);
   const [choosenModal, setChoosenModal] = useState({})
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isCompareSelected, setIsCompareSelected] = useState(false);
+  const [arrayChamp, setArrayChamp] = useState([]);
 
 useEffect(() => {
   fetch('https://ddragon.leagueoflegends.com/cdn/9.13.1/data/pt_BR/championFull.json')
@@ -15,13 +18,24 @@ useEffect(() => {
   .then(values =>  setData(Object.values(values["data"])))
 }, [])
 
+useEffect(() => {
+  console.log(arrayChamp)
+}, [arrayChamp])
+
 function showChampions(currentArray){
   return currentArray.map((champ, index) => {
     const source = "https://ddragon.leagueoflegends.com/cdn/9.13.1/img/champion/" + champ["image"].full;
-    return <ChampIcon id={champ["id"]} key={index} text={champ["name"]} imgsrc={source} onClick={() => {setModalIsOpen(true); setChoosenModal(champ)}}/>
+    return <ChampIcon id={champ["id"]} key={index} text={champ["name"]} imgsrc={source} 
+    onClick={() => { isCompareSelected ? compare(champ) : setModalIsOpen(true); setChoosenModal(champ);}}/>
   })
 }
     
+function compare(champ) {
+  const hasAlready = arrayChamp.findIndex(current => current.id === champ.id);
+  hasAlready !== -1 ? 
+  arrayChamp.splice(hasAlready, 1) : setArrayChamp(() => { return [...arrayChamp, { ...champ}] })
+}
+
   return (
     <div className="div-main">
       <nav className="header-nav">
@@ -31,6 +45,8 @@ function showChampions(currentArray){
           <li><a className="nav-li" href="https://signup.br.leagueoflegends.com/pt/signup/index#/" target="_blank" rel="noopener noreferrer">Jogue!</a></li>
         </ul>
       </nav>
+      <button onClick={() => setIsCompareSelected(!isCompareSelected)}>compare</button>
+      <Chart chartValues={arrayChamp}></Chart>
       <section className="champion-list">
         {showChampions(data)}
       </section>
