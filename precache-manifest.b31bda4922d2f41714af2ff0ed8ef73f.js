4ee8e7fa870276c9e060f3b4a256517f83529cec
Marisa <ma-ah02@live.com>
2020-10-15 15:25:46 -0300

    Updates

---
+++ precache-manifest.b31bda4922d2f41714af2ff0ed8ef73f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffd7a492128000050571202e13cca68",
+    "url": "/data-lovers/index.html"
+  },
+  {
+    "revision": "9f534cb56453b29dbf89",
+    "url": "/data-lovers/static/css/main.0cf303a3.chunk.css"
+  },
+  {
+    "revision": "67b3054b3ffc79666e32",
+    "url": "/data-lovers/static/js/2.e4c83cc7.chunk.js"
+  },
+  {
+    "revision": "9f534cb56453b29dbf89",
+    "url": "/data-lovers/static/js/main.79cf6f69.chunk.js"
+  },
+  {
+    "revision": "e6ac008b23ae6ca9584e",
+    "url": "/data-lovers/static/js/runtime~main.69edb20b.js"
+  }
+]);
