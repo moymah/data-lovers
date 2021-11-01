ef5489ff697f43ab0fcf10ff47c043105534a5b1
Marisa Alves <46823713+moymah@users.noreply.github.com>
2021-10-31 22:51:43 -0300

    Update ChampIcon.js

--- src/components/ChampIcon.js
+++ src/components/ChampIcon.js
@@ -4,10 +4,10 @@ import './ChampIcon.css'
 export default function (props) {
     return (
         <section className="champion" onClick={props.onClick}>
-        <img src ={props.imgsrc} id={props.id} alt={props.text} className="champ-img" />
-        <section>
-          <p htmlFor={props.id} className="champ-name">{props.text}</p>
+            <img src ={props.imgsrc} id={props.id} alt={props.text} className="champ-img" />
+            <section>
+              <p htmlFor={props.id} className="champ-name">{props.text}</p>
+            </section>
         </section>
-      </section>
     );
 }
